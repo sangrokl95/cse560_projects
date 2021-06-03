53dd60c6e71e934095484abe6cc7294d6700a3da
sangrokl95 <54677428+sangrokl95@users.noreply.github.com>
2021-06-02 20:01:10 -0400

    Delete q9.sql

--- q9.sql
+++
@@ -1,5 +0,0 @@
-select t.emp_no, count(*) as cnt from titles as t
-	left join salaries as s on t.emp_no = s.emp_no and t.from_date = s.from_date
-where s.emp_no is null
-group by t.emp_no
-order by t.emp_no
