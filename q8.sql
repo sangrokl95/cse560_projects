ef40f497f744883b0a88851ae0403a09210414aa
sangrokl95 <54677428+sangrokl95@users.noreply.github.com>
2021-06-02 20:01:04 -0400

    Delete q8.sql

--- q8.sql
+++
@@ -1,5 +0,0 @@
-select d.dept_name, count(dm.emp_no) as cnt from (select * from dept_manager where year(to_date) <> '9999') as dm
-	inner join departments as d on dm.dept_no = d.dept_no
-group by dm.dept_no
-having cnt > 1
-order by d.dept_name;
